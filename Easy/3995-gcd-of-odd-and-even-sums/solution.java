79b96f8b0a17562f8029933ef3db2a70935ccbb4
Rishabh Raj <146673683+rishabh26raj@users.noreply.github.com>
2026-07-15 19:57:29 +0530

    Add: 3995. GCD of Odd and Even Sums (Easy) [java]

---
+++ Easy/3995-gcd-of-odd-and-even-sums/solution.java
@@ -0,0 +1,10 @@
+// Problem: 3995. GCD of Odd and Even Sums
+// Difficulty: Easy
+// Language: Java
+// Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
