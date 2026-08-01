0a461c719d0fb74766d7ce36865776cf15807fcb
Rishabh Raj <146673683+rishabh26raj@users.noreply.github.com>
2026-08-01 11:21:02 +0530

    Add: 486. Predict the Winner (Medium) [java]

---
+++ Medium/0486-predict-the-winner/solution.java
@@ -0,0 +1,20 @@
+// Problem: 486. Predict the Winner
+// Difficulty: Medium
+// Language: Java
+// Link: https://leetcode.com/problems/predict-the-winner/
+
+class Solution {
+    Integer[][] dp;
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+        return solve(nums,0,n-1) >= 0;
+    }
+    int solve(int[] nums,int i,int j){
+        if(i==j) return nums[i];
+        if(dp[i][j]!=null) return dp[i][j];
+        int left = nums[i]-solve(nums,i+1,j);
+        int right = nums[j]-solve(nums,i,j-1);
+        return dp[i][j]=Math.max(left,right);
+    }
+}
