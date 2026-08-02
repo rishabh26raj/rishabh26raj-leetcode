d3311b5837d627326764633acc12d922cb14062a
Rishabh Raj <146673683+rishabh26raj@users.noreply.github.com>
2026-08-02 16:14:36 +0530

    Update: 909. Stone Game (Medium) [java]

--- Medium/0909-stone-game/solution.java
+++ Medium/0909-stone-game/solution.java
@@ -8,3 +8,19 @@ class Solution {
         return true;
     }
 }
+
+// Integer[][] dp;
+//     public boolean stoneGame(int[] piles) {
+//         int n = piles.length;
+//         dp = new Integer[n][n];
+//         return solve(piles, 0, n - 1) > 0;
+//     }
+//     private int solve(int[] piles, int i, int j) {
+//         if (i == j)
+//             return piles[i];
+//         if (dp[i][j] != null)
+//             return dp[i][j];
+//         int left = piles[i] - solve(piles, i + 1, j);
+//         int right = piles[j] - solve(piles, i, j - 1);
+//         return dp[i][j] = Math.max(left, right);
+//     }
