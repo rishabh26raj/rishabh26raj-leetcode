8ca8297ebf872736e446c2ce149575cf4a8395e9
Rishabh Raj <146673683+rishabh26raj@users.noreply.github.com>
2026-08-02 16:13:04 +0530

    Add: 909. Stone Game (Medium) [java]

---
+++ Medium/0909-stone-game/solution.java
@@ -0,0 +1,10 @@
+// Problem: 909. Stone Game
+// Difficulty: Medium
+// Language: Java
+// Link: https://leetcode.com/problems/stone-game/
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
